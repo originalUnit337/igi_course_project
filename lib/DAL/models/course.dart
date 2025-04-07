b421915431b559603948e6deb830a5c4c0f65b98
Unit337 <devguyvf@gmail.com>
2025-04-07 23:48:41 +0300

    Deleted deprecated (old, useless) model course

--- lib/DAL/models/course.dart
+++
@@ -1,9 +0,0 @@
-class Course {
-  final int _id;
-  final String _language;
-  final List<Map<String, String>> _grammer_tasks;
-  final List<Map<String, Map<String, String>>> _reading_tasks;
-  final List<Map<String, Map<String, String>>> _audition_tasks;
-  Course(this._id, this._language, this._grammer_tasks, this._reading_tasks, this._audition_tasks);
-}
-

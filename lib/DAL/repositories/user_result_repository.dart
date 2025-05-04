fc5f40b986fb79126c85f911660d899e274d2e66
Unit337 <devguyvf@gmail.com>
2025-05-04 15:01:06 +0300

    some improve

--- lib/DAL/repositories/user_result_repository.dart
+++ lib/DAL/repositories/user_result_repository.dart
@@ -5,8 +5,7 @@ import 'package:igi_course_project/DAL/models/user_result/user_result.dart';
 class UserResultRepository {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
-  Future<void> saveUserResult(
-      String courseId, UserResult userResult) async {
+  Future<void> saveUserResult(String courseId, UserResult userResult) async {
     // await _firestore
     //     .collection('Courses')
     //     .doc(courseId)
@@ -18,7 +17,13 @@ class UserResultRepository {
     // await _firestore.collection('Courses').doc(courseId).collection('userResults')
     // .doc(userResult.userId)
     // .set(userResult.toJson());
-    await _firestore.collection('userResults').add(userResult.toJson());
+    String documentId =
+        '${userResult.userId}_${courseId}_${userResult.lessonName}';
+    await _firestore
+        .collection('userResults')
+        .doc(documentId)
+        .set(userResult.toJson());
+    //await _firestore.collection('userResults').add(userResult.toJson());
     // .add(userResult
     //     .toJson()); // Используем add для создания нового документа
   }
@@ -36,14 +41,22 @@ class UserResultRepository {
     //   return UserResult.fromJson(doc.data() as Map<String, dynamic>);
     // }
     // return null;
-    QuerySnapshot querySnapshot = await _firestore
-        .collection('Courses')
-        .doc(courseId)
-        .collection('Lessons')
-        .doc(lessonId)
-        .collection('userResults')
-        .get();
+    // QuerySnapshot querySnapshot = await _firestore
+    //     .collection('Courses')
+    //     .doc(courseId)
+    //     .collection('Lessons')
+    //     .doc(lessonId)
+    //     .collection('userResults')
+    //     .get();
 
+    // return querySnapshot.docs.map((doc) {
+    //   return UserResult.fromJson(doc.data() as Map<String, dynamic>);
+    // }).toList();
+    CollectionReference results = _firestore.collection('userResults');
+    QuerySnapshot querySnapshot = await results
+        .where('courseId', isEqualTo: courseId)
+        .where('lessonId', isEqualTo: lessonId)
+        .get();
     return querySnapshot.docs.map((doc) {
       return UserResult.fromJson(doc.data() as Map<String, dynamic>);
     }).toList();
